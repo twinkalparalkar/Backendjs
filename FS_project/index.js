19413dd7791cb658267656de180e27fc9d40657e
twinkalp <twinkalparalkar2@gmail.com>
2024-07-04 13:43:44 +0530

    backend route Setup

--- FS_project/index.js
+++ FS_project/index.js
@@ -13,20 +13,27 @@ app.get('/',(req,res)=>{
     fs.readdir(`./files`,(err, files)=>{
         console.log(files)
         res.render("index",{files:files})//ejs file and sending data object to ejs
-})
-
     })
-    
-//"/author/:home" is route  :home is dynamic part
-app.get("/author/:username",(req,res)=>{
-    res.send(`Welcome, ${req.params.username}`);
-    // res.render("index")//ejs file
 })
 
-app.get("/author/:username/:age",(req,res)=>{
-    res.send(`Welcome, ${req.params.username} of age ${req.params.age}`);
-    // res.render("index")//ejs file
+app.post('/create',(req,res)=>{
+    console.log(req.body)
+    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`,req.body.details,(err)=>{
+        console.log(err)
+        res.redirect("/")
+    })
 })
+
+//"/author/:home" is route  :home is dynamic part
+// app.get("/author/:username",(req,res)=>{
+//     res.send(`Welcome, ${req.params.username}`);
+//     // res.render("index")//ejs file
+// })
+
+// app.get("/author/:username/:age",(req,res)=>{
+//     res.send(`Welcome, ${req.params.username} of age ${req.params.age}`);
+//     // res.render("index")//ejs file
+// })
 
 app.listen(3000,()=>{
     console.log("Server is running")
